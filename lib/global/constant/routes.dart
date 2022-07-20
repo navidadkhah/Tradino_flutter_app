3e63ee6b34f53d5fb98b2e321c9e35e726cb71fa
nawid <nnd1380@gmail.com>
2022-07-20 17:40:10 +0430

    create privacy folder

--- lib/global/constant/routes.dart
+++ lib/global/constant/routes.dart
@@ -1,12 +1,14 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:tradino_flutter/screens/login/login_bindings.dart';
 import 'package:tradino_flutter/screens/login/login_screen.dart';
+import 'package:tradino_flutter/screens/privacy/privacy_screen.dart';
 import 'package:tradino_flutter/screens/signup/signup_bindings.dart';
 import 'package:tradino_flutter/screens/signup/signup_screen.dart';
 
 class Routes {
   static const kSignup = '/signup';
   static const kLogin = '/login';
+  static const kPrivacy = '/privacy';
 
   static final pages = [
     GetPage(
@@ -18,6 +20,11 @@ class Routes {
       name: kLogin,
       page: () => const LoginScreen(),
       binding: LoginBindings(),
-    )
+    ),
+    GetPage(
+      name: kPrivacy,
+      page: () => const PrivacyScreen(),
+      binding: LoginBindings(),
+    ),
   ];
 }
---
+++ lib/screens/privacy/privacy_bindings.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class PrivacyBindings extends Bindings {
+  @override
+  void dependencies() {
+    //Get.lazyPut(() => ApiController());
+  }
+}
