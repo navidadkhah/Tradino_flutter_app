d8713ce559024d97ed497898ff6dd53386e4de9f
nawid <nnd1380@gmail.com>
2022-07-20 15:17:48 +0430

    fix shadow widget

---
+++ lib/global/constant/shadows.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tradino_flutter/global/constant/colors.dart';
+
+final kInputContainerShadow = BoxShadow(
+  color: Colors.black12,
+  spreadRadius: 0.0.r,
+  blurRadius: 10.0.r,
+  offset: Offset(0.0.w, 4.0.h),
+);
+
+final kButtonWidgetShadow1 = BoxShadow(
+  color: kWhite,
+  blurRadius: 10.0.r,
+  spreadRadius: 0.0.r,
+  offset: Offset(-5.0.w, -5.0.h),
+);
+
+final kButtonWidgetShadow2 = BoxShadow(
+  color: kEerieBlack,
+  blurRadius: 10.0.r,
+  spreadRadius: 0.0.r,
+  offset: Offset(5.0.w, 5.0.h),
+);
