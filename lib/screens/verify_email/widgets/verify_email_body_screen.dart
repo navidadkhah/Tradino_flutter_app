838fb72c6671db33903e20ffb91fbde16e1d4d9b
nawid <nnd1380@gmail.com>
2022-07-27 13:15:31 +0430

    add pinput package and use it for verification code

--- lib/screens/verify_email/widgets/verify_email_body_screen.dart
+++ lib/screens/verify_email/widgets/verify_email_body_screen.dart
@@ -1,11 +1,13 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:tradino_flutter/global/constant/colors.dart';
+import 'package:tradino_flutter/global/constant/shadows.dart';
 import 'package:tradino_flutter/global/constant/text_styles.dart';
 import 'package:tradino_flutter/global/widgets/button_widget.dart';
 import 'package:tradino_flutter/screens/forgot_password/forgot_password_controller.dart';
 import 'package:pinput/pinput.dart';
+import 'package:tradino_flutter/screens/verify_email/verify_email_controller.dart';
 
 class VerifyEmailBodyWidget extends StatelessWidget {
   const VerifyEmailBodyWidget({Key? key}) : super(key: key);
@@ -13,6 +15,7 @@ class VerifyEmailBodyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ForgotPasswordController email = Get.find();
+    final VerifyEmailController controller = Get.find();
 
     return Expanded(
       flex: 5,
@@ -29,9 +32,9 @@ class VerifyEmailBodyWidget extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height: 24.0.h),
+              SizedBox(height: 36.0.h),
               Text(
-                "Verification your email !",
+                "Verification your email!",
                 style: kSemiBoldCharcoal24,
               ),
               SizedBox(height: 24.0.h),
@@ -49,12 +52,37 @@ class VerifyEmailBodyWidget extends StatelessWidget {
                   )
                 ],
               ),
-              SizedBox(height: 32.0.h),
-              Text(
-                "Resend code",
-                style: kSemiBoldRoyalBlue16,
+              SizedBox(
+                child: Pinput(
+                  androidSmsAutofillMethod:
+                      AndroidSmsAutofillMethod.smsRetrieverApi,
+                  length: 4,
+                  controller: controller.verifyTextController,
+                  autofocus: true,
+                  defaultPinTheme: PinTheme(
+                    margin: EdgeInsets.symmetric(horizontal: 4.0.w),
+                    width: 52.0.w,
+                    height: 72.0.h,
+                    textStyle: kNormalBlack18,
+                    decoration: BoxDecoration(
+                        color: kWhite,
+                        border: Border.all(style: BorderStyle.none),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          kButtonWidgetShadow3,
+                        ]),
+                  ),
+                ),
               ),
-              SizedBox(height: 128.0.h),
+              SizedBox(height: 32.0.h),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Resend code",
+                  style: kSemiBoldRoyalBlue16,
+                ),
+              ),
+              SizedBox(height: 80.0.h),
               ButtonWidget(
                 width: double.infinity,
                 height: 52.0.h,
