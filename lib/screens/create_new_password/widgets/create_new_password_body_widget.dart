754a31a98bd685c79c2fe151d6ad98cdb5363ea2
nawid <nnd1380@gmail.com>
2022-07-30 13:10:43 +0430

    fix some bugs

--- lib/screens/create_new_password/widgets/create_new_password_body_widget.dart
+++ lib/screens/create_new_password/widgets/create_new_password_body_widget.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tradino_flutter/global/constant/colors.dart';
 import 'package:tradino_flutter/global/constant/text_styles.dart';
